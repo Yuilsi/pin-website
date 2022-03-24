e000fee89d464ae58d88eb035603368198cde227
Yuilsi <yuilsimadeleine@gmail.com>
2022-03-23 20:06:38 -0500

    Correction of the js code for a better performance. The data load was performed again and the filter was changed to a forEach

--- arrays.js
+++ arrays.js
@@ -1,10 +1,11 @@
-const object = [
+const objects = [
     {
          name: "corgi",
          price:5500,
          kind:"gothic",
          description:"es un pin xxx",
          material: "metalico",
+         stock:false
    },
     {
         name: "gato",
@@ -12,6 +13,7 @@ const object = [
         kind:"anime character",
         description:"es un pin xxx",
         material: "metalico",
+        stock:true
     },
     {
         name: "conejo",
@@ -19,6 +21,7 @@ const object = [
         kind:"dog",
         description:"es un pin xxx",
         material: "metalico",
+        stock:true
     },
     {
         name: "anime",
@@ -26,18 +29,44 @@ const object = [
         kind:"cat",
         description:"es un pin xxx",
         material: "metalico",
+        stock:false
+       
     }, 
    ];
 
-  const listPines = object.filter(function(expensivePin) { 
-      if(expensivePin.price > 6000){
-          return true
-      }
+   const divobjects= document.getElementById("objects");
 
-  })
+   objects.forEach(product => {
+       const {name,price,kind,description,material,stock}=product;
+      if(price > 6000 || stock){
+       console.log(product);
+      } 
 
-  console.log(listPines)
-   
-  function expensivePin() {
-    console.log(listPines)
-  }
+      const pines= document.createElement("div");
+      const stockLabel = stock ? "ON STROCK" : " NOT ON STROCK";
+
+      pines.innerHTML=  `
+      <h1 class= "product__name">${name}</h1>
+      <h2 class= "product__kind">${kind}</h2>
+      <h3 class= "product__material">${material}</h3>
+      <p>${description}</p>
+      <p class="product__price">${price}</p>
+      <p>${stockLabel}</p>
+      `;
+      
+      divobjects.appendChild(pines);
+
+
+   });
+
+
+
+
+
+
+
+
+
+
+
+ 
