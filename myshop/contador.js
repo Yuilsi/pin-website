1fe68cfa9c495aff8ff196d2f6fa22a181f7d2b4
Yuilsi <yuilsimadeleine@gmail.com>
2022-05-14 21:40:14 -0500

    Removal of unnecessary files from the repo

---
+++ myshop/contador.js
@@ -0,0 +1,16 @@
+const btnAumentar=document.querySelector('.btn-info')
+        const btnDisminuir=document.querySelector('.btn-danger')
+        const span = document.getElementById('span')
+        let contador = 0
+        btnAumentar.addEventListener('click', ()=>{
+            console.log('me diste click aumentar')
+            contador++
+            span.textContent= contador
+        })
+
+        btnDisminuir.addEventListener('click', ()=>{
+            console.log('me diste click disminuir')
+            contador--
+            span.textContent= contador
+            //console-log(disminuir)
+        })
