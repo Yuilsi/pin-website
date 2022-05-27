00d925a884771cbbfc6c1c8e6be940ff53eaf884
lauraduza9857 <80287236+lauraduza9857@users.noreply.github.com>
2022-05-27 00:26:30 -0500

    Run code

--- myshop/src/scripts/scrollImages.js
+++ myshop/src/scripts/scrollImages.js
@@ -1,54 +1,56 @@
- import Sketch from './module';
+import Sketch from './module';
 
-let sketch = new Sketch ({
-    dom:document.getElementById("container")
+let sketch = new Sketch({
+    dom: document.getElementById("container")
 });
 
 let speed = 0;
 let position = 0;
+let rounded = 0;
 let block = document.getElementById('block')
 let wrap = document.getElementById('wrap')
 let elems = [...document.querySelectorAll('.n')];
-window.addEventListener( 'wheel',(e)=>{
- console.log(e); 
- speed += e.deltaY*0.0002; 
+window.addEventListener('wheel', (e) => {
+    console.log(e);
+    speed += e.deltaY * 0.0003;
 })
- 
-let objs = Array(5).fill({dist:0})
 
-function raf (){
+let objs = Array(4).fill({ dist: 0 });
+console.log(sketch);
+
+function raf() {
     position += speed;
-    speed *=0.8;
-
+    speed *= 0.8;
+    
     //desplazamiento
-    objs.forEach((o,i)=>{
-     o.dist = Math.min(Math.abs(position - i),1)
-     o.dist = 1 - o.dist**2;
+    objs.forEach((o, i) => {
+        o.dist = Math.min(Math.abs(position - i), 1)
+        o.dist = 1 - o.dist ** 2;
 
-     //probando una escena loca de escalar
-     elems[i].style.transform = `scale(${1 + 0.4*o.dist})`
-        let scale = 1 + 0.1*o.dist;
-        sketch.meshes[i].position.y = i*1.2 + position*1.2
-        sketch.meshes[i].scale.set(scale,scale,scale)
+        //probando una escena loca de escalar
+        elems[i].style.transform = `scale(${1 + 0.4 * o.dist})`
+        let scale = 1 + 0.1 * o.dist;
+
+        sketch.meshes[i].position.y = - i * 1.2 + position * 1.2;
+        sketch.meshes[i].scale.set(scale, scale, scale);
         sketch.meshes[i].material.uniforms.distanceFromCenter.value = o.dist;
 
-   
+
 
 
     })
     rounded = Math.round(position);
-    
-    let diff= (rounded - position);
-    position += Math.sign(diff)*Math.pow(Math.abs(diff),0.7)*0.035;
-    /* block.style.transform = `translate(0,${position*100 + 50}px)` */
-    wrap.style.transform = `translate(0,${-position*100 + 50}px)`;
-   /*  sketch.meshes.forEach((mesh,i)=>{
-        mesh.position.y = i*1.2 + position*1.2
-        mesh.scale.set(i*1.2 +position*1.2)
 
-    }) */
+    let diff = (rounded - position);
+    position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.7) * 0.015;
+    /* block.style.transform = `translate(0,${position*100 + 50}px)` */
+    wrap.style.transform = `translate(0,${-position * 100 + 50}px)`;
+    /*  sketch.meshes.forEach((mesh,i)=>{
+         mesh.position.y = i*1.2 + position*1.2
+         mesh.scale.set(i*1.2 +position*1.2)
+ 
+     }) */
     window.requestAnimationFrame(raf)
 }
 
 raf();
- 
--- myshop/src/scripts/shaders/fragment.glsl
+++ myshop/src/scripts/shaders/fragment.glsl
@@ -5,15 +5,11 @@ uniform sampler2D texture1;
 uniform vec4 resolution;
 varying vec2 vUv;
 varying vec3 vPosition;
-float PI = 3,1415926535897932;
+float PI = 3.1415926535897932;
 
 
 void main(){
-    
-    vec4 t = texture2D(texture1,vUv);
-    float bw = (t.r + t.b + t.g)/3.;
-    vec4 another = vec4(bw,bw,bw,1.);
-    gl_Fragcolor = mix(another,t, distanceFromCenter);
-    gl_Fragcolor.a = clamp(distanceFromCenter,0.2,1.);
+    vec4 t = texture2D(texture1,vUv)*distanceFromCenter;
+    gl_FragColor = t;
 
 }
